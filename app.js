26f78c164d4b9451270f7b67ce4a4afdcb45b575
dick <503434074@qq.com>
2018-02-23 01:58:35 +0800

    dick

--- app.js
+++ app.js
@@ -2,16 +2,31 @@
 App({
   onLaunch: function () {
     // 展示本地存储能力
-    var logs = wx.getStorageSync('logs') || []
+    /*var logs = wx.getStorageSync('logs') || []
     logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
+    wx.setStorageSync('logs', logs)*/
 
     // 登录
     wx.login({
       success: res => {
+        console.log(res.code);
+        /*wx.request({
+          url: 'https://7e.7-event.cn/d/ci/7e/ApiWeChat/loginAction/6',
+          data: {code: res.code},
+          method: 'POST',
+          dataType: 'json',
+          success: function(res){
+            console.log(res);
+          },
+          fail: function(e){
+            console.log(e.errMsg)
+          }
+        });*/
         // 发送 res.code 到后台换取 openId, sessionKey, unionId
       }
     })
+
+
     // 获取用户信息
     wx.getSetting({
       success: res => {
@@ -19,14 +34,15 @@ App({
           // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
           wx.getUserInfo({
             success: res => {
+              console.log(res);
               // 可以将 res 发送给后台解码出 unionId
-              this.globalData.userInfo = res.userInfo
+              /*this.globalData.userInfo = res.userInfo
 
               // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
               // 所以此处加入 callback 以防止这种情况
               if (this.userInfoReadyCallback) {
                 this.userInfoReadyCallback(res)
-              }
+              }*/
             }
           })
         }
@@ -34,6 +50,7 @@ App({
     })
   },
   globalData: {
+    step: 0,
     userInfo: null
   }
 })
