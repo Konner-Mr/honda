cd9b60d1eb327564e0f9bce16e9eaae498815146
dick <503434074@qq.com>
2018-02-23 10:00:23 +0800

    dick

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 //app.js
 App({
+  globalData: {
+    step: 0,
+    userInfo: null
+  },
   onLaunch: function () {
     // 展示本地存储能力
     /*var logs = wx.getStorageSync('logs') || []
@@ -10,18 +14,9 @@ App({
     wx.login({
       success: res => {
         console.log(res.code);
-        /*wx.request({
-          url: 'https://7e.7-event.cn/d/ci/7e/ApiWeChat/loginAction/6',
-          data: {code: res.code},
-          method: 'POST',
-          dataType: 'json',
-          success: function(res){
-            console.log(res);
-          },
-          fail: function(e){
-            console.log(e.errMsg)
-          }
-        });*/
+        this.requestAction('https://7e.7-event.cn/d/ci/7e/ApiWeChat/loginAction/6', { code: res.code }, function (res){
+          console.log(res);
+        })
         // 发送 res.code 到后台换取 openId, sessionKey, unionId
       }
     })
@@ -34,7 +29,7 @@ App({
           // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
           wx.getUserInfo({
             success: res => {
-              console.log(res);
+              //console.log(res);
               // 可以将 res 发送给后台解码出 unionId
               /*this.globalData.userInfo = res.userInfo
 
@@ -49,8 +44,19 @@ App({
       }
     })
   },
-  globalData: {
-    step: 0,
-    userInfo: null
+  requestAction:function(url,data,callback){
+    wx.request({
+      url: url,
+      data: data,
+      header: {'content-type':'application/x-www-form-urlencoded'},
+      method: 'POST',
+      dataType: 'json',
+      success: function (res) {
+        typeof callback === 'function' && callback(res.data);
+      },
+      fail: function (e) {
+        console.log(e.errMsg)
+      }
+    });
   }
 })
